0af301168d750b571911309c947c6ad21edf8947
sopoour <sophia.auer@googlemail.com>
2025-05-16 14:01:54 +0200

    comment out impressum and privacy policy

--- src/pages/impressum.tsx
+++ src/pages/impressum.tsx
@@ -7,17 +7,17 @@ import { FC } from 'react';
 import useSWR from 'swr';
 
 const Impressum: FC = () => {
-  const { data: generalContentData, isLoading } = useSWR<GeneralContent | null>(
+  /* const { data: generalContentData, isLoading } = useSWR<GeneralContent | null>(
     '/api/generalContent',
     fetcher,
     {},
-  );
-  if (isLoading) {
+  ); */
+  /* if (isLoading) {
     return (
       <section style={{ background: 'linear-gradient(150deg, #bfc2e2 0%, #F4F6FB 80%)' }}>
         <Container size="md" py="150px">
           <Stack gap={'40px'}>
-            <Skeleton height={40} width="60%" radius="sm" mt={32} /> {/* Title */}
+            <Skeleton height={40} width="60%" radius="sm" mt={32} /> 
             <Skeleton height={20} width="80%" />
             <Skeleton height={20} width="70%" />
             <Skeleton height={20} width="50%" />
@@ -30,11 +30,11 @@ const Impressum: FC = () => {
         </Container>
       </section>
     );
-  }
+  } */
   return (
     <BackgroundSection id="impressum" background="linear-gradient(150deg, #bfc2e2 0%, #F4F6FB 80%)">
       <Container size="md" py={{ base: '80px', sm: '150px' }}>
-        <MarkdownConfig content={generalContentData?.impressum as string} />
+        {/* <MarkdownConfig content={generalContentData?.impressum as string} /> */}
       </Container>
     </BackgroundSection>
   );
