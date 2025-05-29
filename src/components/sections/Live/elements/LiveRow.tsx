ea61dcba209c0bde6c63cc38b9555344f43cca8d
sopoour <sophia.auer@googlemail.com>
2025-05-29 13:15:40 +0200

    all white font

--- src/components/sections/Live/elements/LiveRow.tsx
+++ src/components/sections/Live/elements/LiveRow.tsx
@@ -1,4 +1,4 @@
-import { ISOToDate } from '@app/utils/formatDate';
+import { ISOToDate, normalizeDate } from '@app/utils/formatDate';
 import { Button, Flex, Text } from '@mantine/core';
 import { FC } from 'react';
 import styles from '../Live.module.scss';
@@ -15,57 +15,62 @@ type Props = {
 };
 
 const LiveRow: FC<Props> = ({ date, location, venue, ticketLink, ticketNotiz }) => {
-  const gridComponent = (isLinked: boolean) => (
-    <Flex
-      direction={{ base: 'column', sm: 'row' }}
-      gap="16px"
-      justify={'space-between'}
-      className={styles.liveRow}
-    >
-      <Flex direction={'column'} gap={{ base: '0', xs: '20px' }}>
-        <Flex gap={'16px'} align={'center'} className={styles.location}>
-          <Text c={'primary.9'} size="40px" tt={'uppercase'} fw={600} ff={'Oswald'}>
-            {location}
-          </Text>
-          <Text c={'primary.0'} size="28px">
-            {venue}
+  const gridComponent = () => {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    return (
+      <Flex
+        direction={{ base: 'column', sm: 'row' }}
+        gap="16px"
+        justify={'space-between'}
+        className={styles.liveRow}
+      >
+        <Flex direction={'column'} gap={{ base: '0', xs: '20px' }}>
+          <Flex gap={'16px'} align={'center'} className={styles.location}>
+            <Text c={'white'} size="40px" tt={'uppercase'} fw={600} ff={'Oswald'}>
+              {location}
+            </Text>
+            <Text c={'white'} size="28px">
+              {venue}
+            </Text>
+          </Flex>
+
+          <Text fw={600} c={'white'} style={{ letterSpacing: '-0.1rem' }} className={styles.date}>
+            {date && ISOToDate(date)}
           </Text>
         </Flex>
 
-        <Text fw={600} c={'white'} style={{ letterSpacing: '-0.1rem' }} className={styles.date}>
-          {date && ISOToDate(date)}
-        </Text>
+        <Flex>
+          {(ticketLink || ticketNotiz) &&
+            (ticketLink ? (
+              <Button
+                variant="filled"
+                component="span"
+                size={'lg'}
+                radius={'xl'}
+                className={styles.ticketButton}
+              >
+                {date && normalizeDate(date) >= today ? 'Ticket' : 'Impressions'}
+              </Button>
+            ) : (
+              <Text c={'primary.9'} size="md">
+                {ticketNotiz}
+              </Text>
+            ))}
+        </Flex>
       </Flex>
-
-      <Flex>
-        {(ticketLink || ticketNotiz) &&
-          (ticketLink ? (
-            <Button
-              variant="filled"
-              component="span"
-              size={'lg'}
-              radius={'xl'}
-              className={styles.ticketButton}
-            >
-              {date && new Date(date).getDate() >= new Date().getDate() ? 'Ticket' : 'Impressions'}
-            </Button>
-          ) : (
-            <Text c={'primary.9'} size="md">
-              {ticketNotiz}
-            </Text>
-          ))}
-      </Flex>
-    </Flex>
-  );
+    );
+  };
 
   return (
     <>
       {ticketLink ? (
         <Link href={ticketLink} target="_blank">
-          {gridComponent(true)}
+          {gridComponent()}
         </Link>
       ) : (
-        gridComponent(false)
+        gridComponent()
       )}
     </>
   );
